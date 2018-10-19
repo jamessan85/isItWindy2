1e325e5c1ae4ee6746de60775f1f3db0bb06efdb
James Sanderson <jamessandersoon@gmail.com>
2018-10-19 07:37:57 +0100

    Added icons for wind

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,16 +3,53 @@ new Vue({
   data: {
     desiredWind: '',
     location: '',
-    theWeather: []
+    theWeather: [],
+    glpyh: ''
   },
-  mounted() {
-    axios.get('/weather')
-      .then(response => {
-      this.theWeather.push(response.data);
-    })
+  async mounted() {
+    try {
+      var result = await axios.get('/weather')
+      this.theWeather.push(result.data);
+      this.getBearingCurrent(result.data.currently.windBearing)
+    } catch (error) {
+      console.log(error)
+    }
   }, 
   methods: {
-   
+    getBearingCurrent: function(bearing) {
+      switch(true) {
+        case (bearing >= 347.5 && bearing <= 22.5):
+          this.glyph = '<i class="fas fa-arrow-left"></i>'
+          break;
+        case (bearing >= 23 && bearing <= 67.5):
+          this.theWeather[0].currently.windBearing = "North East"
+          break;
+        case (bearing >= 68 && bearing <= 112.5):
+          this.theWeather[0].currently.windBearing = '<i class="fas fa-arrow-left"></i>'
+          break;
+        case (bearing >= 68 && bearing <= 112.5):
+          this.theWeather[0].currently.windBearing = '<i class="fas fa-arrow-left"></i>'
+          break;
+        case (bearing >= 113 && bearing <= 157.5):
+          this.glyph = 'fas fa-arrow-left fa-3x'
+          break;
+        case (bearing >= 158 && bearing <= 202.5):
+          this.theWeather[0].currently.windBearing = "South"
+          break;
+        case (bearing >= 68 && bearing <= 112.5):
+          this.theWeather[0].currently.windBearing = '<i class="fas fa-arrow-left"></i>'
+          break;
+        case (bearing >= 68 && bearing <= 112.5):
+          this.theWeather[0].currently.windBearing = '<i class="fas fa-arrow-left"></i>'
+          break;
+        default:
+          bearing = "Not Available"
+          break;
+      }
+    }
+  },
+  computed: {
+    
   }
 })
 
