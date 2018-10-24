dc9fc25e479171409740f403daaea4e1c5a38332
James Sanderson <jamessandersoon@gmail.com>
2018-10-24 07:47:41 +0100

    Added arrows

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,44 +4,46 @@ new Vue({
     desiredWind: '',
     location: '',
     theWeather: [],
-    glpyh: ''
+    glyph: ''
   }, 
   methods: {
     getBearingCurrent: function(bearing) {
       switch(true) {
         case (bearing >= 348 && bearing <= 22.5): //north
-          this.glyph = 'fas fa-arrow-left fa-3x arrow'
+          this.glyph = 'fas fa-arrow-left fa-3x arrow fromN'
           break;
         case (bearing >= 23 && bearing <= 67.5): // North East
-          this.glyph = "North East"
+          this.glyph = 'fas fa-arrow-left fa-3x arrow fromNE'
           break;
         case (bearing >= 68 && bearing <= 112.5): // East
           this.glyph = 'fas fa-arrow-left fa-3x arrow'
           break;
         case (bearing >= 113 && bearing <= 157.5): // South East
-          this.glyph = 'fas fa-arrow-left fa-3x arrow'
+          this.glyph = 'fas fa-arrow-left fa-3x arrow fromSE'
           break;
         case (bearing >= 157 && bearing <= 202.5): // South
-          this.glyph = 'fas fa-arrow-left fa-3x arrow'
+          this.glyph = 'fas fa-arrow-left fa-3x arrow fromS'
           break;
         case (bearing >= 203 && bearing <= 247.5):// South West
-          this.glyph = "South"
+          this.glyph = 'fas fa-arrow-left fa-3x arrow fromSW'
           break;
         case (bearing >= 248 && bearing <= 302.5):// West
-          this.glyph = 'fas fa-arrow-left fa-3x arrow'
+          this.glyph = 'fas fa-arrow-left fa-3x arrow fromW'
           break;
         case (bearing >= 303 && bearing <= 347.5): //North West
-          this.glyph = 'fas fa-arrow-left fa-3x arrow'
+          this.glyph = 'fas fa-arrow-left fa-3x arrow fromNW'
           break;
         default:
-          bearing = "Not Available"
+          this.glyph = "Not-Available"
           break;
       }
     },
     getWeather: async function() {
       try {
         var result = await axios.get('/weather/' + this.location)
-        console.log(result);
+        this.theWeather.push(result.data);
+        console.log(this.theWeather[0].currently.windBearing)
+        this.getBearingCurrent(this.theWeather[0].currently.windBearing)
       } catch (error) {
         console.log(error)
       }
